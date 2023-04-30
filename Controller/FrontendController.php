5e17bb1725483c995166a6433f8dae0081cf0af7
HuynhHuuLoc8888 <113402222+HuynhHuuLoc8888@users.noreply.github.com>
2023-04-30 16:57:53 +0700

    link url

--- Controller/FrontendController.php
+++ Controller/FrontendController.php
@@ -4,11 +4,27 @@
         $act=$_GET['act'];
     }
     switch($act){
-        case 'gioithieu':
-            include "./View/front/gioithieu_trangchu.php";
-            break;
         case 'trangchu':
             include "./View/front/home.php";
             break;
+        case 'gioithieu':
+            include "./View/front/gioithieu_trangchu.php";
+            break;
+        case 'doanhnghiep':
+            include "./View/front/doanhnghiep_trangchu.php";
+            break;
+        case 'dautu':
+            include "./View/front/dautu_trangchu.php";
+            break;
+        case 'giayphep':
+            include "./View/front/giayphep_trangchu.php";
+            break;
+        case 'sohuutritue':
+            include "./View/front/sohuutritue_trangchu.php";
+            break;
+        case 'vuviecvuan':
+            include "./View/front/vuviec-vuan_trangchu.php";
+            break;
+
     }
 ?>
